65d599451d0298edc2fa488a420772cc7474dd2d
seongwook cheon <blossom090315@naver.com>
2022-05-11 03:53:29 +0900

    master first commit

---
+++ src/main/java/com/mycom/team/HomeController.java
@@ -0,0 +1,28 @@
+package com.mycom.team;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	public SqlSession sqlSession;
+	
+	//메인
+	@RequestMapping(value = "/index")
+	public String index()
+	{
+		return "main";
+	}
+	
+}
---
+++ src/main/java/com/mycom/team/Service.java
@@ -0,0 +1,5 @@
+package com.mycom.team;
+
+public class Service {
+
+}
