4b0411c0ec38a20bc1780262c70b9efdd4e8d003
Youngseok Lee <myejrrp123@naver.com>
2022-05-12 18:02:12 +0900

    seok 1 commit

--- src/main/java/com/mycom/team/Service.java
+++ src/main/java/com/mycom/team/Service.java
@@ -1,7 +1,12 @@
 package com.mycom.team;
 
+import java.util.ArrayList;
+
 public interface Service {
 	
    public void insert(String id,String pw,String name,String tel,String email,String address);
-   
+   /*
+   public ArrayList<SangDTO> out();
+   public ArrayList<SangDTO> detail(String gifttitle);  
+   */
 }
